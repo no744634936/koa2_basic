3a97f5838a2c07d712bef57de1a4a16d9631ceee
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-03-28 23:01:42 +0900

    first commit

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
-
   const Koa = require('koa')
-  const router = require('koa-router')()
+  const router=require("./router")
+  const bodyParser = require('koa-bodyparser')
   const app = new Koa()
+  app.use(bodyParser())
 
-  router.get('/', async(ctx, next) => {
-    ctx.response.body = `<h1>index page</h1>`
-  })
-
-  router.get('/home', async(ctx, next) => {
-    console.log(ctx.request.query)
-    console.log(ctx.request.querystring)
-    ctx.response.body = '<h1>HOME page</h1>'
-  })
-
-  router.get('/404', async(ctx, next) => {
-    ctx.response.body = '<h1>404 Not Found</h1>'
-  })
-
-  // add router middleware:
-  app.use(router.routes())
-
+  router(app);
+  
   app.listen(3000, () => {
     console.log('server is running at http://localhost:3000')
   })
